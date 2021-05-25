0b439ac58da3b4d2a4a2fbe9049b197418893dd7
DevyBadjarad <devybadjarad99@gmail.com>
2021-05-25 23:08:13 +0800

    second

---
+++ app/src/main/java/id/ac/unhas/mynote/AppDataBase.kt
@@ -0,0 +1,31 @@
+package id.ac.unhas.mynote
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], exportSchema = false, version = 1)
+abstract class AppDataBase : RoomDatabase() {
+    abstract fun noteDao() : NoteDao
+
+    companion object {
+        private const val DB_NAME = "MYNOTE_DB"
+        private var instance: AppDataBase? = null
+
+        fun getInstance(context: Context) : AppDataBase? {
+            if (instance==null) {
+                synchronized(AppDataBase::class) {
+                    instance = Room
+                        .databaseBuilder(
+                            context,
+                            AppDataBase::class.java,
+                            DB_NAME
+                        )
+                        .build()
+                }
+            }
+            return instance
+        }
+    }
+}
